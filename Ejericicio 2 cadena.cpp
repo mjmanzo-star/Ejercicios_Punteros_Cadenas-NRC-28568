f2e3f0d832436b5df7d7fbb0aae29a30deafc2ed
majo <mjmanzo@espe.edu.ec>
2026-07-02 20:40:08 -0500

    Ejercicio 2_Cadena.cpp
    Imprimir números del 1 al 4 de tres formas

---
+++ Ejericicio 2 cadena.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+
+    char numeroUno[] = "1";
+    char numeroDos[] = "2";
+    char numeroTres[] = "3";
+    char numeroCuatro[] = "4";
+
+    printf("%s %s %s %s\n", numeroUno, numeroDos, numeroTres, numeroCuatro);
+
+    printf("%s %s %s %s\n", numeroUno, numeroDos, numeroTres, numeroCuatro);
+
+    printf("%s ", numeroUno);
+    printf("%s ", numeroDos);
+    printf("%s ", numeroTres);
+    printf("%s\n", numeroCuatro);
+
+    return 0;
+}
